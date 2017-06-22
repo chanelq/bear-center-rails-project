c94c8d4268fbeceb75cac418044fc079786e88a0
Chanel Quisol <chanel.quisol@gmail.com>
2017-06-22 12:41:08 -0700

    created employees via seed file, added drop down menu of employees to new order form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Employee.create!([
+  {name: 'Jay Adams', signup_date: DateTime.now},
+  {name: 'Drake Jones', signup_date: DateTime.now},
+  {name: 'Linda Williams', signup_date: DateTime.now},
+  {name: 'Marie Henderson', signup_date: DateTime.now}
+])
