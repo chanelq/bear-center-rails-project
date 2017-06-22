a4fa81aee6647a536de5ef911a6f33acfb4398be
Chanel Quisol <chanel.quisol@gmail.com>
2017-06-22 13:02:55 -0700

    created function to calculate average total price, displayed in employee table

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,12 @@
 class Employee < ApplicationRecord
 	has_many :orders, dependent: :destroy
+
+	def calc_average
+		vals = []
+		self.orders.each do |i|
+			vals.push(i.total_price)
+		end
+
+		vals.inject(0.0) { |sum, el| sum + el } / vals.size
+	end
 end
